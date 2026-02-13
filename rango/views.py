1b6460b334be011249f2177b9eba1fcd6552dde0
nominsunshine <gantulganominerdene5@gmail.com>
2026-02-13 00:49:38 +0000

    Chapter 4: add templates and render index page

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hello world")
+    context_dict = {}
+    return render(request, 'rango/index.html', context=context_dict)
