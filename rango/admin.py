66e55f309eac9bb3a076423c57cd60ddba8949e6
nominsunshine <gantulganominerdene5@gmail.com>
2026-02-13 09:49:08 +0000

    Chapter 6: category pages and category links

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
+
+# This makes slug auto-fill from name
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+# Register models with admin
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
